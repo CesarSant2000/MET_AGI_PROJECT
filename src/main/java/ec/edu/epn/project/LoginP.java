c0bd571ba7e9d4eaafec93d0b180abaa0e3caec7
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2021-12-15 00:44:39 -0500

    Initial commit

---
+++ src/main/java/ec/edu/epn/project/LoginP.java
@@ -0,0 +1,52 @@
+package ec.edu.epn.project;
+
+import ec.edu.epn.project.Usuario;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LoginP {
+    private String username;
+    private String password;
+    private ArrayList<Usuario> usuarios;
+    private Usuario usuarioActual;
+
+    public void obtenerLogin(){
+        Scanner inputUsername = new Scanner(System.in);
+        System.out.print("Ingresar usuario: ");
+        username = inputUsername.nextLine();
+        Scanner inputPassword = new Scanner(System.in);
+        System.out.print("Ingresar contraseña: ");
+        password = inputPassword.nextLine();
+    }
+
+    public void completarLogin(){
+        obtenerLogin();
+        int temp1 = 0;
+        for(Usuario usr : usuarios){
+            if(usr.getUsername().equals(getUsername()) && usr.getPassword().equals(getPassword())){
+                usuarioActual = usr;
+                temp1 = 1;
+                break;
+            }
+        }if(temp1 == 0){
+            completarLogin();
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+}
---
+++ src/main/java/ec/edu/epn/project/Menu.java
@@ -0,0 +1,9 @@
+package ec.edu.epn.project;
+
+import java.util.ArrayList;
+
+public class Menu {
+    public ArrayList<String> generarListaGerente(){
+        return null;
+    }
+}
