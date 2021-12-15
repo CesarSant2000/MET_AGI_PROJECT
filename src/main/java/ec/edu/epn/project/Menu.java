70a7b49b943ef17f1b985ed7bc06698d1e680540
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2021-12-15 16:08:41 -0500

    Creación ProjectExecute

--- src/main/java/ec/edu/epn/project/Menu.java
+++ src/main/java/ec/edu/epn/project/Menu.java
@@ -3,11 +3,16 @@ package ec.edu.epn.project;
 import java.util.ArrayList;
 
 public class Menu {
+    public ArrayList<String> opciones = new ArrayList<>();
     private final Usuario usuario;
     public Menu(Usuario usuario) {
         this.usuario = usuario;
     }
     public ArrayList<String> generarListaGerente(){
-        return null;
+        opciones.add("0. Salir");
+        opciones.add("1. Crear producto");
+        opciones.add("2. Actualizar producto");
+        opciones.add("2. Eliminar producto");
+        return opciones;
     }
 }
---
+++ src/main/java/ec/edu/epn/project/ProjectExecute.java
@@ -0,0 +1,8 @@
+package ec.edu.epn.project;
+
+public class ProjectExecute {
+    public static void main(String[] args) {
+        System.out.println("Santacruz, Santamaría, Velastegui");
+        System.out.println("Implementación US1");
+    }
+}
