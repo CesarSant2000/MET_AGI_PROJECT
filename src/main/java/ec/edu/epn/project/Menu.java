1821fcc61100f6f12a7809b1c50844c68a73100c
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2021-12-15 00:48:24 -0500

    First Test

--- src/main/java/ec/edu/epn/project/Menu.java
+++ src/main/java/ec/edu/epn/project/Menu.java
@@ -3,6 +3,10 @@ package ec.edu.epn.project;
 import java.util.ArrayList;
 
 public class Menu {
+    private final Usuario usuario;
+    public Menu(Usuario usuario) {
+        this.usuario = usuario;
+    }
     public ArrayList<String> generarListaGerente(){
         return null;
     }
---
+++ src/test/java/ec/edu/epn/project/MenuTest.java
@@ -0,0 +1,20 @@
+package ec.edu.epn.project;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MenuTest {
+    @Test
+    public void given_user_when_manager_then_manager_options(){
+        Usuario gerente = new Usuario("us1", "pas1", "Maria", "Caseres","Gerente");
+        Menu miMenu = new Menu(gerente);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("0. Salir");
+        expected.add("1. Crear producto");
+        ArrayList<String> actual = miMenu.generarListaGerente();
+        assertEquals(actual, expected);
+    }
+}
